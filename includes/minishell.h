13d564295c4e7ae5b3c1cd1662096c38fde79ff6
Hugo Gutterres <hgutterr@c2r5s13.42lisboa.com>
2025-12-16 15:12:23 +0000

    created minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,8 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgutterr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 13:46:47 by hgutterr          #+#    #+#             */
+/*   Updated: 2025/12/16 14:11:38 by hgutterr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+
+# include "minishell_macros.h"
+# include "minishell_structs.h"
 
 #endif
