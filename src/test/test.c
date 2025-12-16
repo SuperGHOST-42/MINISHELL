958cd42cd34419a2138ca06ec8a54cf1f05aed2b
SuperGHOST-42 <ariclenesp12@gmail.com>
2025-12-16 22:29:23 +0000

    add test2.c

--- src/test/test.c
+++ src/test/test.c
@@ -3,29 +3,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void fatal(const char *msg)
+static void perror_exit(const char *msg)
 {
     perror(msg);
     exit(1);
 }
-int	main(void)
+
+int main(void)
 {
-	int		fd_p[2];
-	pid_t 	pid1;
-	pid_t 	pid2;
-	
+  int p[2];
+  pid_t pid1;
+  char *buf;
+  int bytes_read;
 
-	pipe(fd_p);
-	pid1 = fork();
+  if (pipe(p) == -1)
+    perror_exit("Pipe fail\n");
+  
+  pid1 = fork();
+  if (pid1 == -1)
+    perror_exit("Fork fail");
+  
+  if (pid1 == 0)
+  {
+    close(p[0]);
+    dup2(p[1], 1);
+    write (1, "ABC", 3);
+    close (p[1]);
+    exit(0);
+  }
+  close (p[1]);
 
-	if (pid1 == 0)
-	{
-		dup2(fd_p[1], 1);
-		write(1, "abc", 3);
-		char line[4];
-		read(fd_p[0], line, 3);
-		printf("line: %s\n", line);
-	}
-
-
+  bytes_read = read(p[0], buf, 3);
+  if (bytes_read > 0 )
+  {
+    buf[bytes_read] = '\0';
+    write (1, buf, 3);
+    close(p[0]);
+  }
+  waitpid(pid1, 0, 0);
+  return (0);
 }
