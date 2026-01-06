97db75bebecaf22aafdf14601474ff50316776eb
Ariclenes Pereira <arpereir@c2r2s5.42lisboa.com>
2026-01-06 14:39:49 +0000

    first commit

--- src/main.c
+++ src/main.c
@@ -121,3 +121,40 @@ const char *token_type_to_str(t_token_type type)
         return ("R_APP");
     return ("UNKNOWN");
 }
+
+/////////////////////////////////////////////////////////////////////
+
+#include "../includes/minishell.h"
+#include <readline/readline.h>
+
+static void error_exit(const char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+char	*ft_getline(void)
+{
+	char cwd[BUFSIZ];
+	char *buf;
+
+	getcwd(cwd, sizeof(cwd));
+	printf("%s", cwd);
+	buf = readline(" $> ");
+	return (buf);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	char *line;
+	
+	while (1)
+	{
+		line = ft_getline();
+		if (line == NULL)
+			break;
+		printf("%s\n", line);
+		// do stuff
+		free(line);
+	}
+}
