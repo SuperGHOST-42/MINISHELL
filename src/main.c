09f0d0145bf6e6a121c425be0ff931f0266d1c59
SuperGHOST-42 <ariclenesp12@gmail.com>
2026-01-08 14:14:31 +0000

    update main.c

--- src/main.c
+++ src/main.c
@@ -1,39 +1,70 @@
-# include "../includes/minishell.h"
+#include "../includes/minishell.h"
+#include <string.h>
 
+static	void error_exit(char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
+void	free_cmd(t_cmd *cmd)
+{
+	int i;
+
+	i = 0;
+	while (cmd->args[i])
+	{
+		free(cmd->args[i]);
+		i++;
+	}
+	free(cmd);
+}
+
+static void print_args(t_cmd *cmd)
+{
+	for(int i = 0; cmd->args[i] != NULL; i++)
+	{
+		printf("%s\n", cmd->args[i]);
+	}
+}
 char	*ft_readline(void)
 {
 	char	cwd[BUFSIZ];
 	char	*prompt;
 	char	*line;
 
-	if (!getcwd(cwd, sizeof(cwd)))
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		printf("error getcwd\n");
 		return (readline(" > "));
-
+	}
 	prompt = ft_strjoin(cwd, " > ");
 	line = readline(prompt);
 	free(prompt);
 	return (line);
 }
-
-static	void	error_exit(char *msg)
+void	parser(t_cmd *cmd)
 {
-	perror("error: ");
-	exit(1);
+	cmd->args = malloc(sizeof(char *) * 3);
+	if(cmd == NULL)
+		error_exit("Malloc failed");
+	
+	cmd->args[0] = ft_strdup("/bin/echo");
+	cmd->args[1] = ft_strdup("Hello World");
+	cmd->args[2] = NULL;
+	cmd->builtin = 1;
+	cmd->pid = -1;
+	cmd->redirs = NULL;
+	cmd->next = NULL;
 }
-
 int	main(int argc, char **argv, char **env)
 {
+	(void)argc;
+	(void)argv;
+
 	t_cmd	*cmd;
 	char *line;
-
-	cmd = malloc(sizeof(t_cmd));
-	if (!cmd)
-		error_exit("malloc error\n");
-	cmd ->args = malloc(sizeof(char*) * 2);
-	cmd ->builtin = BI_NONE;
-	cmd ->pid = 0;
-	cmd ->redirs = NULL;
-	cmd ->next = NULL; 
+	pid_t pid;
 	
 	while (1)
 	{
@@ -43,5 +74,18 @@ int	main(int argc, char **argv, char **env)
 		else
 			add_history(line);
 		free(line);
+		////////////////////////////
+		cmd = malloc(sizeof(t_cmd));
+		parser(cmd); // hugo
+		pid = fork();
+		if (pid == 0)
+		{
+			execve(cmd->args[0], cmd->args, env);
+			error_exit("execv failed");
+		}
+		free_cmd(cmd);
 	}
+	waitpid(pid, NULL, 0);
+	perror("");
+	return (EXIT_SUCCESS);
 }
