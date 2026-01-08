6fc026a4dd775eca13a79e92ff39782abf39b5df
SuperGHOST-42 <ariclenesp12@gmail.com>
2026-01-08 00:03:48 +0000

    inicial main

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,58 +1,74 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: arpereir <arpereir@student.42lisboa.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/16 13:46:47 by hgutterr          #+#    #+#             */
-/*   Updated: 2026/01/06 13:40:32 by arpereir         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-<<<<<<< HEAD
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include <string.h>
-# include <errno.h>
-
 # include "libft/libft.h"
-# include "minishell_structs.h"
+# include <unistd.h>
+# include <stdio.h>
 
-// token.c
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	R_IN, 			// <
+	R_OUT,			// >
+	R_APP,			// >>
+	R_HEREDOC		// <<
+}	t_token_type;
 
-t_token 	*new_token(t_token_type type, char *value);
-void 		token_add_back(t_token **lst, t_token *new);
-void 		handle_word(t_token **tokens, char *line, int *i);
-int			handle_quote(t_token **tokens, char *line, int *i);
-void 		handle_operator(t_token **tokens, char *line, int *i);
+typedef enum e_builtin
+{
+	BI_NONE = 0,
+	BI_ECHO,
+	BI_CD,
+	BI_PWD,
+	BI_EXPORT,
+	BI_UNSET,
+	BI_ENV,
+	BI_EXIT
+}	t_builtin;
 
-// tokenization
-t_token 	*tokenization(char *line);
+typedef struct s_token
+{
+	t_token_type			type;
+	char					*value;
+	int						quoted; // "" ''
+	int						squoted; // ''
+	int						dquoted; // ""
+	struct s_token			*next;
+}	t_token;
 
-// syntax_checker.c
+typedef struct s_redirs
+{
+	t_token_type			type;
+	char					*target;
+	int						expand; // HEREDOC expansion;
+	int						heredoc_fd; // -1 if not heredoc
+	struct s_redirs			*next;
+}	t_redirs;
 
-int 		syntax_check(t_token *tokens);
-int 		ft_isredir(t_token_type type);
+typedef struct s_cmd
+{
+	char 					**args;
+	t_redirs		 		*redirs;
+	t_builtin				is_builtin;
+	pid_t					pid;
+	struct s_cmd			*next;
+}	t_cmd;	
 
-// exit.c
+typedef struct s_env
+{
+	char					*key; 		// "PATH"
+	char					*value;		// "/usr/bin:/bin:..."
+	int						has_value; 	// 1 if has value, 0 if just key
+	struct s_env			*next;
+}	t_env;
 
-int			exit_shell(t_token *tokens, int exit_code);
-void		free_tokens(t_token *tokens);
-=======
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-
->>>>>>> ghost
+typedef struct s_shell
+{
+	t_env		*env;			// linked list of environment variables
+	int			last_status;	// $? (0..255)
+	int 		should_exit;	// flag to indicate if shell should exit
+	int 		exit_code;		// exit code to use when exiting
+}	t_shell;
 
 #endif
