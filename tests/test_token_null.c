ae17aa26851a20fa44ece761045f0e600be6bc32
babayagaboy <hugo.guterres.2005@gmail.com>
2026-01-13 16:36:49 +0000

    parsing almost done

--- tests/test_token_null.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/minishell.h"
-
-int main(void)
-{
-    t_token *list = NULL;
-    /* Should not crash or modify list */
-    token_add_back(&list, NULL);
-    return (list != NULL); /* return 0 on success */
-}
