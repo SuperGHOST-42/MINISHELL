ea16caac4cc9ae906b890471086d1de16dcc220f
SuperGHOST-42 <ariclenesp12@gmail.com>
2026-01-27 23:18:30 +0000

    add execcute_builtin.c

---
+++ src/execute_builtin.c
@@ -0,0 +1,12 @@
+#include "../includes/minishell.h"
+
+int exec_builtin(t_cmd *cmd, t_shell *shell)
+{
+	if (cmd->builtin == BI_PWD)
+		ft_pwd();
+	else if (cmd->builtin == BI_ENV)
+		ft_env(shell);
+	else if (cmd->builtin == BI_EXIT)
+		ft_exit(shell, cmd->args[1]);
+	return (77);
+}
