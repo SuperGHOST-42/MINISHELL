4a8b13f944f45b636baa0b1fe2c6289d35278189
SuperGHOST-42 <ariclenesp12@gmail.com>
2026-02-19 18:48:00 +0000

    Integrate ghost execute_pipeline.c

--- src/execution/execute_pipeline.c
+++ src/execution/execute_pipeline.c
@@ -1,11 +1,5 @@
 #include "../../includes/minishell.h"
 
-static void	close_fd(int fd)
-{
-	if (fd >= 0)
-		close(fd);
-}
-
 static int	pipe_prepare(int fd[2], int has_next)
 {
 	fd[0] = -1;
@@ -13,68 +7,71 @@ static int	pipe_prepare(int fd[2], int has_next)
 	if (!has_next)
 		return (0);
 	if (pipe(fd) < 0)
-		return (perror("pipe"), -1);
+	{
+		perror("pipe");
+		return (-1);
+	}
 	return (0);
 }
 
-static pid_t	fork_or_error(void)
+static void	close_fd(int fd)
 {
-	pid_t	pid;
-
-	pid = fork();
-	if (pid < 0)
-		perror("minishell: fork");
-	return (pid);
+	if (fd >= 0)
+		close(fd);
 }
 
-static void	child_setup_io(int prev_read, int fd[2], int has_next)
+static void	clean_fds(int fd[2])
 {
-	if (prev_read >= 0)
+	close_fd(fd[0]);
+	close_fd(fd[1]);
+}
+
+static int	dup_prepare(int fd[2], int prev_fd, int has_next)
+{
+	if (prev_fd >= 0)
 	{
-		if (dup2(prev_read, STDIN_FILENO) < 0)
-		{
-			perror("dup2 stdin");
-			exit(1);
-		}
-		close_fd(prev_read);
+		if (dup2(prev_fd, STDIN_FILENO) < 0)
+			return (clean_fds(fd), perror("dup2 stdin"), -1);
+		close_fd(prev_fd);
 	}
 	if (has_next)
 	{
 		if (dup2(fd[1], STDOUT_FILENO) < 0)
-		{
-			perror("dup2 stdout");
-			exit(1);
-		}
-		close_fd(fd[0]);
+			return (clean_fds(fd), perror("dup2 stdout"), -1);
 		close_fd(fd[1]);
+		close_fd(fd[0]);
 	}
+	return (0);
 }
 
-static void	parent_keep_fds(int *prev_read, int fd[2], int has_next)
+static void	execve_fail(char *cmd, char *path, char **envp)
+{
+	int	code;
+
+	code = 126;
+	if (errno == ENOENT)
+		code = 127;
+	perror(cmd);
+	free(path);
+	free_envp(envp);
+	exit(code);
+}
+
+static void	refresh_fds(int *prev_read, int fd[2], int has_next)
 {
 	close_fd(*prev_read);
+	close_fd(fd[1]);
 	if (has_next)
-	{
-		close_fd(fd[1]);
 		*prev_read = fd[0];
-	}
 	else
 		close_fd(fd[0]);
 }
 
-static void	run_child(t_cmd *cmd, t_shell *shell)
+static void	run_external(t_cmd *cmd, t_shell *shell)
 {
 	char	*path;
 	char	**envp;
 
-	if (!cmd)
-		exit(1);
-	if (apply_redirs(cmd->redirs))
-		exit(1);
-	if (!cmd->args || !cmd->args[0])
-		exit(0);
-	if (cmd->builtin != BI_NONE)
-		exit(exec_builtin(cmd, shell));
 	envp = env_to_envp(shell->env);
 	if (!envp)
 		exit(1);
@@ -86,62 +83,72 @@ static void	run_child(t_cmd *cmd, t_shell *shell)
 		exit(127);
 	}
 	execve(path, cmd->args, envp);
-	perror(path);
-	free(path);
-	free_envp(envp);
-	if (errno == ENOENT)
-		exit(127);
-	exit(126);
+	execve_fail(cmd->args[0], path, envp);
 }
 
-static int	wait_all(t_cmd *cmds, pid_t last_pid)
+static void	run_child(t_cmd *cmd, t_shell *shell)
 {
-	int		st;
-	int		last;
-
-	last = 0;
-	while (cmds)
-	{
-		waitpid(cmds->pid, &st, 0);
-		if (cmds->pid == last_pid)
-			last = st;
-		cmds = cmds->next;
-	}
-	if (WIFEXITED(last))
-		return (WEXITSTATUS(last));
-	if (WIFSIGNALED(last))
-		return (128 + WTERMSIG(last));
-	return (1);
+	if (!cmd)
+		exit(1);
+	if (apply_redirs(cmd->redirs))
+		exit(1);
+	if (!cmd->args || !cmd->args[0])
+		exit(0);
+	if (cmd->builtin != BI_NONE)
+		exit(exec_builtin(cmd, shell));
+	run_external(cmd, shell);
 }
 
-void	exec_pipeline(t_cmd *cmds, t_shell *shell)
+static pid_t	open_process(t_cmd *cur, t_shell *shell, int prev_read, int fd[2])
 {
-	int		prev_read;
-	int		fd[2];
 	pid_t	pid;
-	pid_t	last_pid;
+
+	pid = fork();
+	if (pid < 0)
+		return (clean_fds(fd), -1);
+	if (pid == 0)
+	{
+		if (dup_prepare(fd, prev_read, (cur->next != NULL)) < 0)
+			exit(1);
+		run_child(cur, shell);
+	}
+	return (pid);
+}
+
+static void	wait_pipeline(t_cmd *cmd, t_shell *shell)
+{
+	int		status;
 	t_cmd	*cur;
 
-	prev_read = -1;
-	last_pid = -1;
-	cur = cmds;
+	cur = cmd;
+	while (cur)
+	{
+		if (cur->pid > 0 && waitpid(cur->pid, &status, 0) > 0)
+			shell->last_status = status_to_exit_code(status);
+		cur = cur->next;
+	}
+}
+
+void	exec_pipeline(t_cmd *cmd, t_shell *shell)
+{
+	int		fd[2];
+	int		prev_fd;
+	pid_t	pid;
+	t_cmd	*cur;
+
+	prev_fd = -1;
+	cur = cmd;
 	while (cur)
 	{
 		if (pipe_prepare(fd, (cur->next != NULL)) < 0)
-			return ;
-		pid = fork_or_error();
+			break ;
+		pid = open_process(cur, shell, prev_fd, fd);
 		if (pid < 0)
-			return ;
-		if (pid == 0)
-		{
-			child_setup_io(prev_read, fd, (cur->next != NULL));
-			run_child(cur, shell);
-		}
+			break ;
+		refresh_fds(&prev_fd, fd, (cur->next != NULL));
 		cur->pid = pid;
-		last_pid = pid;
-		parent_keep_fds(&prev_read, fd, (cur->next != NULL));
 		cur = cur->next;
 	}
-	close_fd(prev_read);
-	shell->last_status = wait_all(cmds, last_pid);
+	close_fd(prev_fd);
+	wait_pipeline(cmd, shell);
 }
